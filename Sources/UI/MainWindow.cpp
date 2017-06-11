92be9159ea838a419b6132a8f5cd41e5e5def49e
Chris Ohk <utilforever@gmail.com>
2017-06-11 22:56:53 +0900

    Update MainWindow - Add comment, implement constructor/destructor

--- Sources/UI/MainWindow.cpp
+++ Sources/UI/MainWindow.cpp
@@ -1,7 +1,31 @@
+/*************************************************************************
+> File Name: MainWindow.cpp
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: Main window UI of snow simulation.
+> Created Time: 2017/06/11
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
 #include "MainWindow.h"
+#include "ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-	: QMainWindow(parent)
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent), m_ui(new Ui::MainWindow)
 {
-	ui.setupUi(this);
+	m_ui->setupUi(this);
+
+	SetupUI();
+
+	this->setWindowTitle("Snow Simulation");
+}
+
+MainWindow::~MainWindow()
+{
+	delete m_ui;
+	m_ui = nullptr;
+}
+
+void MainWindow::SetupUI()
+{
+	
 }
--- Sources/UI/MainWindow.h
+++ Sources/UI/MainWindow.h
@@ -1,15 +1,34 @@
-#pragma once
+/*************************************************************************
+> File Name: MainWindow.h
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: Main window UI of snow simulation.
+> Created Time: 2017/06/11
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef SNOW_SIMULATION_MAIN_WINDOW_H
+#define SNOW_SIMULATION_MAIN_WINDOW_H
 
 #include <QtWidgets/QMainWindow>
-#include "ui_MainWindow.h"
+
+// Forward declaration
+namespace Ui
+{
+	class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
 	Q_OBJECT
 
 public:
-	MainWindow(QWidget *parent = Q_NULLPTR);
+	explicit MainWindow(QWidget* parent = nullptr);
+	~MainWindow();
 
 private:
-	Ui::MainWindow ui;
+	void SetupUI();
+
+	Ui::MainWindow* m_ui;
 };
+
+#endif
