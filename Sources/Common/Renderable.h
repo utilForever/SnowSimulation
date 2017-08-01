0f6270e3be06d0e5f3b3e23761d7c7759b3cb458
Chris Ohk <utilforever@gmail.com>
2017-08-01 16:58:42 +0900

    Upload Renderable.h - The common renderable header file of snow simulation

---
+++ Sources/Common/Renderable.h
@@ -0,0 +1,14 @@
+/*************************************************************************
+> File Name: Renderable.cpp
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: The common renderable header file of snow simulation.
+> Created Time: 2017/08/01
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef SNOW_SIMULATION_RENDERABLE_H
+#define SNOW_SIMULATION_RENDERABLE_H
+
+
+
+#endif
