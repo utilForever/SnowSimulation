aad0c701df465075dcce50cdefbd2a9428e60792
Chris Ohk <utilForever@gmail.com>
2017-12-28 13:13:05 +0900

    Update ViewPanel - Add header, preprocessor and some code

--- Sources/UI/ViewPanel.cpp
+++ Sources/UI/ViewPanel.cpp
@@ -1,4 +1,14 @@
+/*************************************************************************
+> File Name: ViewPanel.cpp
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: View panel of snow simulation.
+> Created Time: 2017/12/28
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
 #include "ViewPanel.h"
+
+#include <QWidget>
 
 ViewPanel::ViewPanel(QWidget *parent) :
 	QGLWidget(QGLFormat(QGL::SampleBuffers), parent)
--- Sources/UI/ViewPanel.h
+++ Sources/UI/ViewPanel.h
@@ -1,7 +1,20 @@
+/*************************************************************************
+> File Name: ViewPanel.h
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: View panel of snow simulation.
+> Created Time: 2017/12/28
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef SNOW_SIMULATION_VIEW_PANEL_H
+#define SNOW_SIMULATION_VIEW_PANEL_H
+
 #include <QGLWidget>
 
 class ViewPanel : public QGLWidget
 {
+	Q_OBJECT
+
 public:
 	ViewPanel(QWidget *parent);
 	virtual ~ViewPanel();
@@ -9,3 +22,5 @@ public:
 	bool StartSimulation();
 	void StopSimulation();
 };
+
+#endif
