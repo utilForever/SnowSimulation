f65dd84be0d17c75053c28f7ab383a35639112aa
Chris Ohk <utilForever@gmail.com>
2017-12-29 19:33:41 +0900

    Upload Math.h - The common math header file of snow simulation

---
+++ Sources/Common/Math.h
@@ -0,0 +1,51 @@
+/*************************************************************************
+> File Name: Math.h
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: The common math header file of snow simulation.
+> Created Time: 2017/12/29
+> Copyright (c) 2017, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef SNOW_SIMULATION_MATH_H
+#define SNOW_SIMULATION_MATH_H
+
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <limits>
+#include <random>
+
+template <typename T>
+bool IsEqual(T a, T b)
+{ 
+    return std::fabs(a - b) < std::numeric_limits<T>::epsilon();
+}
+
+template <typename T>
+bool IsNotEqual(T a, T b)
+{
+    return !IsEqual(a, b);
+}
+
+static float UniformRandom(float min = 0.f, float max = 1.f)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(min, max);
+
+    return dis(gen);
+}
+
+static float SmoothStep(float value, float edge0, float edge1)
+{
+    float x = std::clamp((value - edge0) / (edge1 - edge0), 0.f, 1.f);
+    return x * x * (3 - 2 * x);
+}
+
+static float SmootherStep(float value, float edge0, float edge1)
+{
+    float x = std::clamp((value - edge0) / (edge1 - edge0), 0.f, 1.f);
+    return x * x * x * (x * (6 * x - 15) + 10);
+}
+
+#endif
