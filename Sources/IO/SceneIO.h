ee45545c68bfb56b41751ad00f9b4e39fc80f343
Chris Ohk <utilForever@gmail.com>
2018-01-08 18:09:15 +0900

    Upload SceneIO - Scene IO of snow simulation

---
+++ Sources/IO/SceneIO.h
@@ -0,0 +1,69 @@
+/*************************************************************************
+> File Name: SceneIO.h
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: Scene IO of snow simulation.
+> Created Time: 2018/01/08
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef SNOW_SIMULATION_SCENE_IO_H
+#define SNOW_SIMULATION_SCENE_IO_H
+
+#include <glm/mat4x4.hpp>
+
+#include <QtXml/QtXml>
+
+#include <iostream>
+
+struct Vector3;
+
+struct SimulationParameters;
+struct ImplicitCollider;
+
+class Scene;
+class Engine;
+class Grid;
+class ParticleSystem;
+
+class SceneIO
+{
+public:
+	SceneIO();
+
+	bool Read(QString fileName, Scene* scene, Engine* engine);
+	bool Write(Scene* scene, Engine* engine);
+
+	QString GetSceneFile();
+	void SetSceneFile(QString fileName);
+
+private:
+	// import functions
+	void ApplySimulationParameters();
+	void ApplyExportSettings();
+	void ApplyParticleSystem(Scene* scene);
+	void ApplyGrid(Scene* scene);
+	void ApplyColliders(Scene* scene, Engine* engine);
+
+	// export functions
+
+	void AppendSimulationParameters(QDomElement root, float timeStep);
+	void AppendParticleSystem(QDomElement root, Scene* scene);
+	void AppendGrid(QDomElement root, Scene* scene);
+	void AppendColliders(QDomElement root, Scene* scene);
+	void AppendExportSettings(QDomElement root);
+
+	// low level DOM node helpers
+	void AppendString(QDomElement node, const QString name, const QString value);
+	void AppendInt(QDomElement node, const QString name, const int i);
+	void AppendFloat(QDomElement node, const QString name, const float f);
+	void AppendVector(QDomElement node, const QString name, const Vector3 v);
+	void AppendDim(QDomElement node, const QString name, const glm::ivec3 iv);
+	void AppendMatrix(QDomElement node, const QString name, const glm::mat4 mat);
+
+	// i.e. /users/jcarberr/Castle
+	QString m_sceneFilePrefix;
+	// XML document
+	QDomDocument m_document;
+};
+
+#endif
