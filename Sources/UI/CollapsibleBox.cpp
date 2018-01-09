b0aca1a96c5ebc0e3e4183663c51403b0a07144f
Chris Ohk <utilForever@gmail.com>
2018-01-09 13:09:48 +0900

    Update CollapsibleBox - Implement member variables and methods

--- Sources/UI/CollapsibleBox.cpp
+++ Sources/UI/CollapsibleBox.cpp
@@ -1,7 +1,92 @@
-#include "CollapsibleBox.h"
+/*************************************************************************
+> File Name: CollapsibleBox.cpp
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: Collapsible box of snow simulation.
+> Created Time: 2018/01/09
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#include <UI/CollapsibleBox.h>
+#include <UI/UISettings.h>
 
-CollapsibleBox::CollapsibleBox(QWidget *widget) :
-	QGroupBox(widget)
+#include <QApplication>
+#include <QLayout>
+#include <QMouseEvent>
+#include <QPalette>
+
+
+CollapsibleBox::CollapsibleBox(QWidget* widget) :
+	QGroupBox(widget), m_rawTitle(""), m_clicked(false), m_collapsed(false)
 {
 	this->setAutoFillBackground(true);
+}
+
+CollapsibleBox::~CollapsibleBox()
+{
+	UISettings::SetSetting(this->objectName() + "IsCollapsed", m_collapsed);
+}
+
+bool CollapsibleBox::IsCollapsed() const
+{
+	return m_collapsed;
+}
+
+void CollapsibleBox::Init()
+{
+	SetCollapsed(UISettings::GetSetting(this->objectName() + "IsCollapsed", false).toBool());
+}
+
+void CollapsibleBox::mousePressEvent(QMouseEvent *event)
+{
+	if (!childrenRect().contains(event->pos()))
+	{
+		QPalette palette = this->palette();
+		palette.setColor(QPalette::Window, palette.dark().color());
+		palette.setColor(QPalette::Button, palette.dark().color());
+		
+		SetWidgetPalette(this, palette);
+		m_clicked = true;
+	}
+}
+
+void CollapsibleBox::mouseReleaseEvent(QMouseEvent*)
+{
+	if (m_clicked == true)
+	{
+		SetCollapsed(!m_collapsed);
+		SetWidgetPalette(this, QApplication::palette());   
+		m_clicked = false;
+	}
+}
+
+void CollapsibleBox::SetCollapsed(bool collapsed)
+{
+	m_collapsed = collapsed;
+	
+	for (int i = 0; i < children().size(); ++i)
+	{
+		children()[i]->setProperty("visible", !collapsed);
+	}
+	
+	if (m_collapsed == true)
+	{
+		setMaximumHeight(1.25 * fontMetrics().height());
+	}
+	else
+	{
+		setMaximumHeight(16777215);
+	}
+	
+	this->setTitle(m_rawTitle);
+}
+
+void CollapsibleBox::SetWidgetPalette(QWidget* widget, const QPalette& palette)
+{
+	widget->setPalette(palette);
+	
+	QList<QWidget*> children = widget->findChildren<QWidget*>();
+	for (int i = 0; i < children.size(); ++i)
+	{
+		SetWidgetPalette(children[i], palette);
+	}
 }
--- Sources/UI/CollapsibleBox.h
+++ Sources/UI/CollapsibleBox.h
@@ -1,7 +1,42 @@
+/*************************************************************************
+> File Name: CollapsibleBox.h
+> Project Name: SnowSimulation
+> Author: Chan-Ho Chris Ohk
+> Purpose: Collapsible box of snow simulation.
+> Created Time: 2018/01/09
+> Copyright (c) 2018, Chan-Ho Chris Ohk
+*************************************************************************/
+#ifndef SNOW_SIMULATION_COLLAPSIBLE_BOX_H
+#define SNOW_SIMULATION_COLLAPSIBLE_BOX_H
+
 #include <QGroupBox>
 
 class CollapsibleBox : public QGroupBox
 {
+	Q_OBJECT
+	Q_PROPERTY(bool collapsed READ IsCollapsed WRITE SetCollapsed)
+
 public:
-	explicit CollapsibleBox(QWidget *parent);
+	explicit CollapsibleBox(QWidget* parent);
+	~CollapsibleBox();
+
+	bool IsCollapsed() const;
+
+	void Init();
+
+public slots:
+	void mousePressEvent(QMouseEvent* event) override;
+	void mouseReleaseEvent(QMouseEvent*) override;
+
+	void SetCollapsed(bool collapsed);
+
+protected:
+	QString m_rawTitle;
+
+	bool m_clicked;
+	bool m_collapsed;
+
+	static void SetWidgetPalette(QWidget* widget, const QPalette& palette);
 };
+
+#endif
